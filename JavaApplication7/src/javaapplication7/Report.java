18b9a46163be1f06d51b7f100d967a22f4f20296
kevinzwu <kevinzw0622@gmail.com>
2020-04-18 15:50:41 -0400

    KevinInitial

--- JavaApplication7/src/javaapplication7/Report.java
+++ JavaApplication7/src/javaapplication7/Report.java
@@ -10,14 +10,17 @@ public class Report {
     public Report(Person P, double B){
         human = P;
         BMI = B;
-        generateReport(P, BMI);
     }
     
-    public void generateReport(){
+    public String generateReport(){
+        String message = "Report for " + human.firstName + " " + human.lastName + "\n Age = " + human.Age + "\n Height = " + human.height + "\n Weight = " + human.weight + "\n BMI = " + BMI;
+        return message;
+        /*
         System.out.println("Generating report for" + human.firstName + " " + human.lastname);
         System.out.println("Age = " + human.Age);
         System.out.println("Height = "+ human.height);
         System.out.pritnln("Weight = " + human.weight);
         System.out.println("BMI = " + BMI);
+        */
     }
 }
